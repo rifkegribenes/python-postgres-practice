c9a93c73763f53fe988e69556a4ad67b727a7f30
rifkegribenes <rifkegribenes@gmail.com>
2018-10-07 15:37:23 -0700

    section 2 age program finished

--- print.py
+++ print.py
@@ -1,2 +1,8 @@
+# First, ask the user for the age and store it in the `age` variable.
+# This variable is a string, because the input() method always returns a string.
 age = input('Enter your age: ')
-print('You have lived for {} seconds.'.format(str(int(age) * 365 * 24 * 60 * 60)))
+
+# Then, print out the information to the user.
+# Here we use the format() method to replace each pair of curly braces
+#       for each argument of the format() method (separated by commas).
+print("You have lived for {} seconds. This corresponds to {} years.".format(int(age) * 365 * 24 * 60 * 60, age))
