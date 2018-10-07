522c6dfe4b8c67cc23e043e27d81988b24b4a335
rifkegribenes <rifkegribenes@gmail.com>
2018-10-07 15:22:49 -0700

    variable types

---
+++ enter.py
@@ -0,0 +1,5 @@
+text = raw_input("type in enter")
+if text == "":
+    print "you pressed enter"
+else:
+    print "you typed some text before pressing enter"
--- print.py
+++ print.py
@@ -1,3 +1,2 @@
-age = 5
-print(age)
-print(age * 365 * 24 * 60 * 60)
+age = input('Enter your age: ')
+print('You have lived for {} seconds.'.format(str(int(age) * 365 * 24 * 60 * 60)))
