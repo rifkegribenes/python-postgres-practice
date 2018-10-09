39a4c7c47e25d51aa53ca4665be6a27857111b0f
rifkegribenes <rifkegribenes@gmail.com>
2018-10-08 21:58:22 -0700

    section 5 student registration system

--- students/app.py
+++ students/app.py
@@ -1,3 +1,63 @@
-sample_set = {3, 5, 9, 1}
+student_list = []
 
-print(sample_set)
+def create_student():
+    name = input('Enter student name: ')
+    student_data = {
+        'name': name,
+        'marks': []
+    }
+
+    return student_data
+
+
+def add_mark(student, mark):
+    student['marks'].append(mark)
+
+
+def calculate_average_mark(student):
+    total = sum(student['marks'])
+    number = len(student['marks'])
+    if number == 0:
+        return 0
+    return total / number
+
+def student_details(student):
+    print("{}, average mark: {}.".format(student['name'], calculate_average_mark(student)))
+
+
+def print_student_list(students):
+    for i, student in enumerate(students):
+        print("ID: {}".format(i))
+        student_details(student)
+
+
+def menu():
+    # Add a student (to student_list)
+    # Add a mark to a student
+    # Print a list of students
+    # Exit the application
+    selection = input("Enter 'p' to print the student list, "
+                      "'s' to add a new student, "
+                      "'a' to add a mark to a student, "
+                      "or 'q' to quit. "
+                      "Enter your selection: ")
+    while selection != 'q':
+        if selection == 'p':
+            print_student_list(student_list)
+        elif selection == 's':
+            student_list.append(create_student())
+        elif selection == 'a':
+            student_id = int(input("Enter the student ID to add a mark to: "))
+            student = student_list[student_id]
+            new_mark = int(input("Enter the new mark to be added: "))
+            add_mark(student, new_mark)
+
+        selection = input("Enter 'p' to print the student list, "
+                          "'s' to add a new student, "
+                          "'a' to add a mark to a student, "
+                          "or 'q' to quit. "
+                          "Enter your selection: ")
+
+menu()
+
+
