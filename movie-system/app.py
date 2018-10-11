25f21c3f1e2e71b8063fe449026abe9b12ed91c5
rifkegribenes <rifkegribenes@gmail.com>
2018-10-10 22:14:54 -0700

    class methods

--- movie-system/app.py
+++ movie-system/app.py
@@ -1,12 +1,6 @@
 from movie import Movie
 from user import User
 
-user = User("Jose")
+user = User.load_from_file('Jose.txt')
 
-my_movie = Movie("The Matrix", "Sci-Fi", False)
-watched_movie = Movie("Coraline", "Children's", True)
-
-user.movies.append(my_movie)
-user.movies.append(watched_movie)
-
-user.save_to_file()
+print(user.movies)
