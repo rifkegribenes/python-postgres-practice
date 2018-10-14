73e8f2e179db9f0d794c4ef916e5edfffb3c7d43
rifkegribenes <rifkegribenes@gmail.com>
2018-10-14 15:38:28 -0700

    remove constants file

--- tweet-analyzer/project/constants.py
+++
@@ -1,6 +0,0 @@
-CONSUMER_KEY = 'Z4Oh7pi6D17cyr32xOGpBCK45'
-CONSUMER_SECRET = 'Ef32lDXmXVzefhUCc9Zz646pfyWWuVlk5aTL5DvgxVf50OnVng'
-
-REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
-ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
-AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
