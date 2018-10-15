6fd941bf30224c2bbfc0edf89c512ace39528cdd
rifkegribenes <rifkegribenes@gmail.com>
2018-10-14 20:46:54 -0700

    twitter access token

--- tweet-analyzer/project/login.py
+++ tweet-analyzer/project/login.py
@@ -15,4 +15,13 @@ request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
 print("Go to the following site in your browser:")
 print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token['oauth_token']))
 
-oauth_verifier = input("What is the PIN?")
+oauth_verifier = input("What is the PIN? ")
+
+token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
+token.set_verifier(oauth_verifier)
+client = oauth2.Client(consumer, token)
+
+response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
+access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+
+print(access_token)
