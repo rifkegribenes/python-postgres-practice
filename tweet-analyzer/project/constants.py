127e73761ee42536684e68930c8900193a05edb7
rifkegribenes <rifkegribenes@gmail.com>
2018-10-14 20:58:28 -0700

    trying again with the .gitignore

--- tweet-analyzer/project/constants.py
+++
@@ -1,6 +0,0 @@
-CONSUMER_KEY = 'hThF03txAt1fLBKvGfCODuNe6'
-CONSUMER_SECRET = 'uzvWbbcxxUZka0fZWN4qcjPhoT5ByPdalUPF0Mih00QF3nj3WP'
-
-REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
-ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
-AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
