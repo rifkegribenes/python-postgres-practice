7b3a48d914301eb489298045a83a4cecab007749
rifkegribenes <rifkegribenes@gmail.com>
2018-10-14 20:29:00 -0700

    twitter auth

--- tweet-analyzer/project/login.py
+++ tweet-analyzer/project/login.py
@@ -10,3 +10,8 @@ if response.status != 200:
     print("An error occurred getting the request token from Twitter")
 
 request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+
+print("Go to the following site in your browser:")
+print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token['oauth_token']))
+
+oauth_verifier = input("What is the PIN?")
